b0187741955063a0a064920fc44ecf5a480a854f
Arianha Bayley <arianhamb@gmail.com>
2020-09-17 00:30:34 +0100

    Created event form up to the point where API integration needs to happen

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../styles/App.css";
 
+import CreateEvent from "./CreateEvent";
+
 function App() {
   return (
     <div className="App">
       <p>Hello World!</p>
+      <CreateEvent />
     </div>
   );
 }
