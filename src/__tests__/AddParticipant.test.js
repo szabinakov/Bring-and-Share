2529158922dae710e6a8ea84af83d48cc004e19e
szabinakov <szabinkov@gmail.com>
2020-09-28 17:08:54 +0100

    updated test files

--- src/__tests__/AddParticipant.test.js
+++ src/__tests__/AddParticipant.test.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent} from "@testing-library/react";
 import AddParticipant from "../components/AddParticipant";
+
+
 
 describe("AddParticipant component", () => {
   const mockId = 123;
@@ -11,14 +13,23 @@ describe("AddParticipant component", () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it("renders the correct form elements", () => {
-    const { getByTestId } = render(<AddParticipant eventId={mockId} />);
+  it("renders the button element to be able to add participants", () => {
+    const { getByTestId } = render(<AddParticipant eventId={mockId}/>);
 
-    expect(getByTestId("name")).toBeValid();
-    expect(getByTestId("participantEmail")).toBeValid();
-    expect(getByTestId("toBring")).toBeValid();
-    expect(getByTestId("dislikes")).toBeValid();
-    expect(getByTestId("dietInfo")).toBeValid();
-    expect(getByTestId("addButton")).toBeInTheDocument();
+    expect(getByTestId("addParticipantNavButton")).toBeInTheDocument();
+
   });
+
+  it('renders the addparticipant form with the correct inputs when the button has been pressed', () => {
+      const {getByTestId}=render(<AddParticipant eventId={mockId}/>)
+      
+      fireEvent.click(getByTestId('addParticipantNavButton'))
+
+      expect(getByTestId('name')).toBeValid()
+      expect(getByTestId('toBring')).toBeValid()
+      expect(getByTestId('dislikes')).toBeValid()
+      expect(getByTestId('dietInfo')).toBeValid()
+      expect(getByTestId('participantEmail')).toBeValid()
+      expect(getByTestId('addParticipantForm')).toBeInTheDocument()
+  })
 });
--- src/__tests__/EventPage.test.js
+++ src/__tests__/EventPage.test.js
@@ -3,17 +3,16 @@ import { render } from "@testing-library/react";
 import EventPage from "../components/EventPage";
 
 describe('EventPage component', () => {
-    const mockId = 1234
+    
 
-    it('it renders correctly', () => {
-
-        const {asFragment} = render (<EventPage eventId={mockId}/>)
+    xit('it renders correctly', () => {
+        const {asFragment} = render (<EventPage props={{match:{params:{eventId:1234}}}}/>)
 
         expect(asFragment()).toMatchSnapshot()
     })
 
     it('renders Participants, and AddParticipant components', () => {
-        const {getByTestId} = render (<EventPage eventId={mockId}/>)
+        const {getByTestId} = render (<EventPage/>)
 
         expect(getByTestId('eventdetails')).toBeInTheDocument()
     })
